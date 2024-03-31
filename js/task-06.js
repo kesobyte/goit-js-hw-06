6c997c9dcfdd5ff2e3704c26702285a00694a804
kesobyte <kimangeloalvarez@gmail.com>
2024-04-01 01:17:08 +0800

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,15 @@
 const textInput = document.getElementById("validation-input");
-// const textLength = document.querySelector('[data-length="6"]');
-const textLength = textInput.getAttribute("data-length");
+const textLength = parseInt(textInput.getAttribute("data-length"));
 
 textInput.addEventListener("blur", (e) => {
-  if (textInput.value.length <= textLength) {
+  if (textInput.value.length < textLength) {
     textInput.setAttribute("class", "invalid");
   } else {
     textInput.setAttribute("class", "valid");
+  }
+
+  //Update to default
+  if (textInput.value === "") {
+    textInput.setAttribute("class", "");
   }
 });
