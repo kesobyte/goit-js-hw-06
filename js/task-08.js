2c1a52c1b7cf9046d8447ba941fe6fcc8624138c
kesobyte <kimangeloalvarez@gmail.com>
2024-04-01 15:52:33 +0800

    update

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector(".login-form");
+const email = document.querySelector('input[name="email"]');
+const pass = document.querySelector('input[name="password"]');
 
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (email.value === "" || pass.value === "") {
+    alert("All fields must be filled in");
+  } else {
+    const userAccount = {
+      email: email.value,
+      pass: pass.value,
+    };
+    console.log(userAccount);
+  }
+
+  loginForm.reset();
+});
