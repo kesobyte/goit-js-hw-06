e7575f5fba0b096232b4df98ab2bff8527ba31ad
kesobyte <kimangeloalvarez@gmail.com>
2024-04-01 18:09:00 +0800

    update

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const boxes = document.querySelector("#boxes");
+const createBox = document.querySelector("[data-create]");
+let number = document.getElementsByTagName("input");
+
+function createBoxes(amount) {
+  for (i = 0; i <= amount; i++) {
+    boxes.createElement("div");
+    boxes.setAttribute("style", "background-color: black");
+    boxes.appendChild(boxes);
+  }
+}
+
+// createBox.addEventListener("click", createBoxes(number.value));
